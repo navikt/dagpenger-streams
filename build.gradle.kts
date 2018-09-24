cf28c4eb72c0badd4264f51c5784ba0d76481a2b
André Roaldseth <andre@roaldseth.net>
2018-09-24 15:27:28 +0200

    Set up as a standalone project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,40 @@
 plugins {
-    kotlin("jvm")
-    id("java-library")
+    "java-library"
+    kotlin("jvm") version "1.2.51"
+    id("com.diffplug.gradle.spotless") version "3.13.0"
+    id("com.palantir.docker") version "0.20.1"
+    id("com.palantir.git-version") version "0.11.0"
 }
 
-val kafkaVersion = "2.0.0"
-val confluentVersion = "4.1.2"
+apply {
+    plugin("com.diffplug.gradle.spotless")
+}
+
+repositories {
+    jcenter()
+    maven(url = "http://packages.confluent.io/maven/")
+}
+
+version = "0.0.1"
+
 val kotlinLoggingVersion = "1.4.9"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation(project(":events"))
-
-    api("org.apache.kafka:kafka-clients:$kafkaVersion")
-    api("org.apache.kafka:kafka-streams:$kafkaVersion")
-    api("io.confluent:kafka-streams-avro-serde:$confluentVersion")
 
     implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
 
-    testCompile(kotlin("test"))
-    testCompile(kotlin("test-junit"))
-    testCompile("junit:junit:4.12")
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("junit:junit:4.12")
+}
+
+spotless {
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        target("*.gradle.kts", "additionalScripts/*.gradle.kts")
+        ktlint()
+    }
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "dagpenger-streams"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        maven(url = "https://dl.bintray.com/gradle/gradle-plugins")
+    }
+}
