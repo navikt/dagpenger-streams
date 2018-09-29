b49d52ffe0c658dd904b2c5b5b00a243bb81878b
André Roaldseth <andre.roaldseth@nav.no>
2018-09-29 22:21:29 +0200

    Add build pipeline

---
+++ Jenkinsfile
@@ -0,0 +1,31 @@
+pipeline {
+  agent any
+
+  stages {
+    stage('Install dependencies') {
+      steps {
+        sh "./gradlew assemble"
+      }
+    }
+
+    stage('Build') {
+      steps {
+        sh "./gradlew check"
+      }
+    }
+
+    stage("Publish") {
+      when {
+        branch "master"
+      }
+
+      steps {
+        archiveArtifacts artifacts: 'build/libs/*.jar', fingerprint: true
+
+        withCredentials([usernamePassword(credentialsId: 'repo.adeo.no', usernameVariable: 'REPO_CREDENTIAL_USR', passwordVariable: 'REPO_CREDENTIAL_PSW')]) {
+          sh "./gradlew -PmavenUser=${env.REPO_CREDENTIAL_USR} -PmavenPassword=${env.REPO_CREDENTIAL_PSW} publish"
+        }
+      }
+    }
+  }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,27 @@ plugins {
     id("java-library")
     kotlin("jvm") version "1.2.51"
     id("com.diffplug.gradle.spotless") version "3.13.0"
+    id("maven-publish")
+}
+
+buildscript {
+    dependencies {
+        classpath("com.cinnober.gradle:semver-git:2.2.0")
+    }
 }
 
 apply {
     plugin("com.diffplug.gradle.spotless")
+    plugin("com.cinnober.gradle.semver-git")
 }
 
 repositories {
     jcenter()
     maven(url = "http://packages.confluent.io/maven/")
+    maven(url = "https://repo.adeo.no/repository/maven-releases/")
 }
 
 group = "no.nav.dagpenger"
-version = "0.0.1"
 
 val kafkaVersion = "2.0.0"
 val confluentVersion = "4.1.2"
@@ -22,7 +30,7 @@ val kotlinLoggingVersion = "1.4.9"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("no.nav.dagpenger:events:0.0.1")
+    implementation("no.nav.dagpenger:events:0.1.2")
 
     api("org.apache.kafka:kafka-clients:$kafkaVersion")
     api("org.apache.kafka:kafka-streams:$kafkaVersion")
@@ -33,6 +41,26 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
     testImplementation("junit:junit:4.12")
+}
+
+publishing {
+    publications {
+        create("default", MavenPublication::class.java) {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            val version = project.version as String
+
+            url = if (version.endsWith("-SNAPSHOT")) {
+                uri("https://repo.adeo.no/repository/maven-snapshots/")
+            } else {
+                uri("https://repo.adeo.no/repository/maven-releases/")
+            }
+        }
+    }
 }
 
 spotless {
