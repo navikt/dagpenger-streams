bf1c561448966fd6374e58e54cab765b8d89281b
Geir A. Lund <tilgeir@gmail.com>
2018-10-15 14:13:59 +0200

    Added prometheus metrics endpoint  # DIGIDAG-148 (#6)
    * Added prometheus metrics endpoint  # DIGIDAG-148
    - Enabled prometheus default exports (memory, threads etc)
    - Changed env name from KAFKA_BOOTSTRAP_SERVERS_URL to KAFKA_BOOTSTRAP_SERVERS

--- src/main/kotlin/no/nav/dagpenger/streams/Service.kt
+++ src/main/kotlin/no/nav/dagpenger/streams/Service.kt
@@ -3,10 +3,14 @@ package no.nav.dagpenger.streams
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
+import io.ktor.response.respondTextWriter
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.exporter.common.TextFormat
+import io.prometheus.client.hotspot.DefaultExports
 import mu.KotlinLogging
 import org.apache.kafka.streams.KafkaStreams
 import java.util.Properties
@@ -16,9 +20,11 @@ private val LOGGER = KotlinLogging.logger {}
 abstract class Service {
     protected abstract val SERVICE_APP_ID: String
     protected abstract val HTTP_PORT: Int
+    private val collectorRegistry: CollectorRegistry = CollectorRegistry.defaultRegistry
 
     private lateinit var streams: KafkaStreams
     fun start() {
+        DefaultExports.initialize()
         naisHttpChecks()
         streams = setupStreams()
         streams.start()
@@ -35,6 +41,12 @@ abstract class Service {
                 }
                 get("/isReady") {
                     call.respondText("READY", ContentType.Text.Plain)
+                }
+                get("/metrics") {
+                    val names = call.request.queryParameters.getAll("name[]")?.toSet() ?: setOf()
+                    call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004)) {
+                        TextFormat.write004(this, collectorRegistry.filteredMetricFamilySamples(names))
+                    }
                 }
             }
         }.start(wait = false)
