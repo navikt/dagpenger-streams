93670923c286c923e4f7901cd10f616ff59bb358
Geir André Lund <geir.andre.lund@nav.no>
2018-11-23 12:15:07 +0100

    Moved schema registry url property instantiating. Removed pitest - this project is config only basically, makes it hard to test.

--- src/main/kotlin/no/nav/dagpenger/streams/Topics.kt
+++ src/main/kotlin/no/nav/dagpenger/streams/Topics.kt
@@ -1,6 +1,7 @@
 package no.nav.dagpenger.streams
 
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde
 import no.nav.dagpenger.events.avro.Behov
@@ -12,45 +13,86 @@ import org.apache.kafka.streams.kstream.Consumed
 import org.apache.kafka.streams.kstream.KStream
 import org.apache.kafka.streams.kstream.Produced
 
-private val serdeConfig = mapOf(
-        AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to
-                (System.getenv("KAFKA_SCHEMA_REGISTRY_URL") ?: "http://localhost:8081")
-)
+private val strings = Serdes.String()
+private val avroBehovSerde = SpecificAvroSerde<Behov>()
+private val genericAvro = GenericAvroSerde()
 
 object Topics {
     val JOARK_EVENTS = Topic(
-            "aapen-dok-journalfoering-v1",
-            keySerde = Serdes.String(),
-            valueSerde = configureGenericAvroSerde(serdeConfig)
+        "aapen-dok-journalfoering-v1",
+        keySerde = strings,
+        valueSerde = genericAvro
     )
 
     val INNGÅENDE_JOURNALPOST = Topic(
-            "privat-dagpenger-journalpost-mottatt-alpha",
-            keySerde = Serdes.String(),
-            valueSerde = configureAvroSerde<Behov>(serdeConfig)
+        "privat-dagpenger-journalpost-mottatt-alpha",
+        keySerde = strings,
+        valueSerde = avroBehovSerde
     )
 }
 
-fun configureGenericAvroSerde(serdeConfig: Map<String, Any>): GenericAvroSerde {
-    return GenericAvroSerde().apply { configure(serdeConfig, false) }
-}
+fun <K : Any, V : GenericRecord> StreamsBuilder.consumeGenericTopic(
+    topic: Topic<K, V>,
+    schemaRegistryUrl: String?
+): KStream<K, V> {
 
-fun <T : SpecificRecord?> configureAvroSerde(serdeConfig: Map<String, Any>): SpecificAvroSerde<T> {
-    return SpecificAvroSerde<T>().apply { configure(serdeConfig, false) }
-}
+    schemaRegistryUrl?.let {
+        topic.keySerde.configure(
+            mapOf(
+                AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl
+            ), true
+        )
 
-fun <K : Any, V : GenericRecord> StreamsBuilder.consumeGenericTopic(topic: Topic<K, V>): KStream<K, V> {
+        topic.valueSerde.configure(
+            mapOf(
+                AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl
+            ), false
+        )
+    }
+
     return stream<K, V>(
-            topic.name, Consumed.with(topic.keySerde, topic.valueSerde)
+        topic.name, Consumed.with(topic.keySerde, topic.valueSerde)
     )
 }
 
-fun <K : Any, V : SpecificRecord> StreamsBuilder.consumeTopic(topic: Topic<K, V>): KStream<K, V> {
+fun <K : Any, V : SpecificRecord> StreamsBuilder.consumeTopic(
+    topic: Topic<K, V>,
+    schemaRegistryUrl: String?
+): KStream<K, V> {
+
+    schemaRegistryUrl?.let {
+        topic.keySerde.configure(
+            mapOf(
+                AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl
+            ), true
+        )
+
+        topic.valueSerde.configure(
+            mapOf(
+                AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl
+            ), false
+        )
+    }
+
     return stream<K, V>(
-            topic.name, Consumed.with(topic.keySerde, topic.valueSerde)
+        topic.name, Consumed.with(topic.keySerde, topic.valueSerde)
     )
 }
 
-fun <K, V> KStream<K, V>.toTopic(topic: Topic<K, V>) {
+fun <K, V> KStream<K, V>.toTopic(topic: Topic<K, V>, schemaRegistryUrl: String?) {
+    schemaRegistryUrl?.let {
+        topic.keySerde.configure(
+            mapOf(
+                AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl
+            ), true
+        )
+
+        topic.valueSerde.configure(
+            mapOf(
+                KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG to true,
+                AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl
+            ), false
+        )
+    }
     return to(topic.name, Produced.with(topic.keySerde, topic.valueSerde))
 }
