9fe72923533d29cc28a3813df5ff597a1a0e37c5
Geir André Lund <geir.andre.lund@nav.no>
2018-11-26 08:14:41 +0100

    version of events should be 0.1.9-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ val fuelVersion = "1.15.0"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("no.nav.dagpenger:events:0.1.6-SNAPSHOT")
+    implementation("no.nav.dagpenger:events:0.1.9-SNAPSHOT")
 
     api("org.apache.kafka:kafka-clients:$kafkaVersion")
     api("org.apache.kafka:kafka-streams:$kafkaVersion")
