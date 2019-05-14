026cd426532b3c4d0edb6a77589b77ec299c156b
Geir André Lund <geir.andre.lund@nav.no>
2019-05-14 06:53:08 +0200

    Bump events to 0.3.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ val moshiVersion = "1.8.0"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("no.nav.dagpenger:events:0.3.8-SNAPSHOT")
+    implementation("no.nav.dagpenger:events:0.3.11-SNAPSHOT")
 
     api("org.apache.kafka:kafka-clients:$kafkaVersion")
     api("org.apache.kafka:kafka-streams:$kafkaVersion")
