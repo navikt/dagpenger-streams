dcbbecdf6b1919c17093c08c3d7d9aec88b1deb3
Giao The Cung <giaothe@gmail.com>
2019-07-02 09:57:50 +0200

    Extract default properties.

--- src/main/kotlin/no/nav/dagpenger/plain/Configs.kt
+++ src/main/kotlin/no/nav/dagpenger/plain/Configs.kt
@@ -14,48 +14,43 @@ import org.apache.kafka.common.serialization.StringSerializer
 import java.io.File
 import java.util.Properties
 
+val defaultConsumerConfig = Properties().apply {
+    put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+    put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+    put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java.name)
+    put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, PacketDeserializer::class.java.name)
+}
+
 fun consumerConfig(
     groupId: String,
     bootstrapServerUrl: String,
-    autoOffsetReset: String = "latest",
-    enableAutoCommit: Boolean = true,
-    keyDeserializer: String? = StringDeserializer::class.java.name,
-    valueDeserializer: String? = PacketDeserializer::class.java.name,
-    credential: KafkaCredential? = null
+    credential: KafkaCredential? = null,
+    properties: Properties = defaultConsumerConfig
 ): Properties {
     return Properties().apply {
+        putAll(properties)
         putAll(commonConfig(bootstrapServerUrl, credential))
         put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
-        put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset)
-        put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommit.toString())
-        keyDeserializer?.let {
-            put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, it)
-        }
-        valueDeserializer?.let {
-            put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, it)
-        }
     }
+}
+
+val defaultProducerConfig = Properties().apply {
+    put(ProducerConfig.ACKS_CONFIG, "1")
+    put(ProducerConfig.BATCH_SIZE_CONFIG, "1")
+    put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+    put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, PacketSerializer::class.java.name)
 }
 
 fun producerConfig(
     clientId: String,
     bootstrapServers: String,
-    acks: String = "1",
-    keySerializer: String? = StringSerializer::class.java.name,
-    valueSerializer: String? = PacketSerializer::class.java.name,
-    credential: KafkaCredential? = null
+    credential: KafkaCredential? = null,
+    properties: Properties = defaultProducerConfig
 ): Properties {
     return Properties().apply {
+        putAll(properties)
         putAll(commonConfig(bootstrapServers, credential))
         put(ProducerConfig.CLIENT_ID_CONFIG, clientId)
-        put(ProducerConfig.ACKS_CONFIG, acks)
-        put(ProducerConfig.BATCH_SIZE_CONFIG, "1")
-        keySerializer?.let {
-            put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, it)
-        }
-        valueSerializer?.let {
-            put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, it)
-        }
     }
 }
 
