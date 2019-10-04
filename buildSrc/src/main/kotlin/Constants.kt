de7e4674e64605f228a23680bc0548c9fa22ce4f
Geir André Lund <geir.andre.lund@nav.no>
2019-10-04 12:42:49 +0200

    Update 3rd party dependencies [skip ci]

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -34,7 +34,7 @@ object Dagpenger {
         const val ktorUtils = "com.github.navikt.dp-biblioteker:ktor-utils:$version"
     }
 
-    const val Streams = "com.github.navikt:dagpenger-streams:2019.08.13-10.25.4b7bef10fc88"
+    const val Streams = "com.github.navikt:dagpenger-streams:2019.10.04-11.54.1af65bdd3862"
     const val Events = "com.github.navikt:dagpenger-events:2019.08.06-10.38.92d9930cd257"
 }
 
