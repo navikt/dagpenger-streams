caf0558bc6c682b6012d1e82f9295c5626bade76
Andreas Bergh <andreas.nygard.bergh@nav.no>
2020-01-08 09:14:20 +0100

    added test with pregenerated json in resources folder

--- src/test/kotlin/no/nav/dagpenger/streams/CompressionPackageTest.kt
+++ src/test/kotlin/no/nav/dagpenger/streams/CompressionPackageTest.kt
@@ -14,6 +14,7 @@ import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.RecordMetadata
+import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.kstream.Predicate
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.KafkaContainer
@@ -22,6 +23,12 @@ import java.util.Properties
 import java.util.concurrent.TimeUnit
 
 private val logger = KotlinLogging.logger { }
+
+private val testTopic =  Topic(
+    "privat-dagpenger-test",
+    keySerde = Serdes.String(),
+    valueSerde = Serdes.serdeFrom(PacketSerializer(), PacketDeserializer())
+)
 
 private object Kafka {
     val instance by lazy {
@@ -32,6 +39,7 @@ private object Kafka {
 private data class TestData(val data: String, val number: Int, val double: Double, val moreData: String) {
     private val jsonAdapter = moshiInstance.adapter(TestData::class.java)
     fun toJson(): Any? = jsonAdapter.toJsonValue(this)
+
     companion object {
         fun generate(): Sequence<TestData> = generateSequence {
             TestData(
@@ -67,8 +75,31 @@ class CompressionPackageTest {
         }
     }
 
+    class TestServiceThatAddBigPregeneratedData : River(testTopic) {
+        override val SERVICE_APP_ID = "TestService2"
+        override val withHealthChecks = false
+
+        override fun filterPredicates(): List<Predicate<String, Packet>> {
+            return listOf(Predicate { _, packet -> !packet.hasField("big-json") })
+        }
+
+        override fun onPacket(packet: Packet): Packet {
+            val largeJson = this::class.java.getResource("/unormalt-stor-json.json").readText()
+            packet.putValue(
+                "big-json", largeJson
+
+            )
+            logger.info { "Packet size ${packet.toJson()?.toUtf8Bytes()?.size}" }
+            return packet
+        }
+
+        override fun getConfig(): Properties {
+            return streamConfig(SERVICE_APP_ID, Kafka.instance.bootstrapServers)
+        }
+    }
+
     @Test
-    fun `Should compress packages`() {
+    fun `Should compress packages with randomly generated json`() {
         val producer = KafkaProducer<String, Packet>(
             producerConfig(
                 clientId = "test",
@@ -102,5 +133,46 @@ class CompressionPackageTest {
 
         packets.size shouldBe 2
         packets.last().value().hasField("big-json")
+
+        producer.close()
+    }
+
+    @Test
+    fun `Should compress packages with pregenerated json`() {
+        val producer = KafkaProducer<String, Packet>(
+            producerConfig(
+                clientId = "test",
+                bootstrapServers = Kafka.instance.bootstrapServers
+            ).also {
+                it[ProducerConfig.ACKS_CONFIG] = "all"
+            })
+
+        val packet = Packet()
+
+        val metaData: RecordMetadata =
+            producer.send(ProducerRecord(testTopic.name, packet)).get(5, TimeUnit.SECONDS)
+        logger.info("Producer produced to topic@offset -> '$metaData'")
+
+        TestServiceThatAddBigPregeneratedData().also { it.start() }
+
+        val consumer = KafkaConsumer<String, Packet>(
+            consumerConfig(
+                groupId = "test2",
+                bootstrapServerUrl = Kafka.instance.bootstrapServers
+            ).also {
+                it[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+            }
+        )
+
+        consumer.subscribe(listOf(testTopic.name))
+
+        TimeUnit.SECONDS.sleep(3)
+
+        val packets = consumer.poll(Duration.ofSeconds(1)).toList()
+
+        packets.size shouldBe 2
+        packets.last().value().hasField("big-json")
+
+        producer.close()
     }
 }
