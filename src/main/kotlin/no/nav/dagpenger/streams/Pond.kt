d31056b4516b7f2c72c9380f8a33e2e76441bedd
Geir André Lund <geir.andre.lund@nav.no>
2020-02-26 07:28:38 +0100

    Adjust log level for rivers and ponds on each packet ref https://nav-it.slack.com/archives/C60FFACN5/p1582667149044100

--- src/main/kotlin/no/nav/dagpenger/streams/Pond.kt
+++ src/main/kotlin/no/nav/dagpenger/streams/Pond.kt
@@ -16,10 +16,10 @@ abstract class Pond(private val topic: Topic<String, Packet>) : Service() {
         val stream = builder.consumeTopic(topic)
         stream
             .peek { _, packet -> ThreadContext.put(CorrelationId.X_CORRELATION_ID, packet.getCorrelationId()) }
-            .peek { key, _ -> LOGGER.info("Pond recieved packet with key $key and will test it against filters.") }
+            .peek { key, _ -> LOGGER.debug { "Pond recieved packet with key $key and will test it against filters." } }
             .filter { key, packet -> filterPredicates().all { it.test(key, packet) } }
             .foreach { key, packet ->
-                LOGGER.info("Packet with key $key passed filters and now calling onPacket() for: $packet")
+                LOGGER.debug { "Packet with key $key passed filters and now calling onPacket() for: $packet" }
 
                 val timer = processTimeLatency.startTimer()
                 onPacket(packet)
