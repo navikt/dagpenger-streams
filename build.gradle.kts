a0dbe5f48b80a887fd3c41172d7d11ac3ff56b1f
André Roaldseth <andre.roaldseth@nav.no>
2020-04-16 15:50:17 +0200

    Fix paths for spotless/ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,7 +116,7 @@ spotless {
         ktlint(Klint.version)
     }
     kotlinGradle {
-        target("*.gradle.kts", "additionalScripts/*.gradle.kts")
+        target("*.gradle.kts", "buildSrc/**/*.kt*")
         ktlint(Klint.version)
     }
 }
