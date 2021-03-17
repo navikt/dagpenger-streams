ed62f8f9a325f2b22a5623cfddafd89ffe3cfa23
Giao The Cung <giaothe@gmail.com>
2021-03-17 08:53:09 +0100

    Default verider for nais og aiven kafka config
    Co-authored-by: Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
    Co-authored-by: kriise <kmriise@gmail.com>
    Co-authored-by: Geir Andre Lund <geir.andre.lund@nav.no>

--- src/main/kotlin/no/nav/dagpenger/streams/KafkaAivenCredentials.kt
+++ src/main/kotlin/no/nav/dagpenger/streams/KafkaAivenCredentials.kt
@@ -7,8 +7,8 @@ data class KafkaAivenCredentials(
     val sslEndpointIdentificationAlgorithmConfig: String = "",
     val sslTruststoreTypeConfig: String = "jks",
     val sslKeystoreTypeConfig: String = "PKCS12",
-    val sslTruststoreLocationConfig: String,
-    val sslTruststorePasswordConfig: String,
-    val sslKeystoreLocationConfig: String,
-    val sslKeystorePasswordConfig: String
+    val sslTruststoreLocationConfig: String = "/var/run/secrets/nais.io/kafka/client.truststore.jks",
+    val sslTruststorePasswordConfig: String = System.getenv("KAFKA_CREDSTORE_PASSWORD"),
+    val sslKeystoreLocationConfig: String = "/var/run/secrets/nais.io/kafka/client.keystore.p12",
+    val sslKeystorePasswordConfig: String = sslTruststorePasswordConfig
 )
